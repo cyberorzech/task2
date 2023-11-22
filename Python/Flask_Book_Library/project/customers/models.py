43d504714aafb6c5b1f46ff9e384a6ff84fb2e72
cyberorzech <natan.orze@gmail.com>
2023-11-22 16:26:13 +0100

    solve task2.1

--- Python/Flask_Book_Library/project/customers/models.py
+++ Python/Flask_Book_Library/project/customers/models.py
@@ -1,8 +1,43 @@
+import re
+from loguru import logger
+from sys import stdout
+
 from project import db, app
+
+class ConfidentialInfoFlusher:
+    def __repr__(self):
+        # keys_blacklist = ["pesel", "street", "city"]
+        keys_whitelist = ["name", "age", "id"]
+        # flushed_info = {k: self.__dict__[k] for k in self.__dict__.keys() - {*keys_blacklist}}
+        flushed_info = {k: self.__dict__[k] for k in self.__dict__.keys() if k in keys_whitelist}
+        return str(flushed_info)
+
+
+def obfuscate_message(message: str):
+    """Obfuscate sensitive information."""
+    SENSITIVE_INFO_REGEX_DICT = {
+        "appNo": "AppNo: [A-Za-z0-9]{1,10}"
+    }
+    for index, regex in enumerate(SENSITIVE_INFO_REGEX_DICT.values()):
+        try:
+            pattern = re.compile(regex)
+            match = pattern.search(message)
+            result = message.replace(match.group(), f"{[*SENSITIVE_INFO_REGEX_DICT.keys()][index]}: ***")
+        except AttributeError:
+            continue
+    return result
+
+def formatter(record):
+    record["extra"]["obfuscated_message"] = obfuscate_message(record["message"])
+    return "[{level}] {extra[obfuscated_message]}\n{exception}"
+
+logger.remove()
+logger.add(stdout, format=formatter)
+
 
 
 # Customer model
-class Customer(db.Model):
+class Customer(ConfidentialInfoFlusher, db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
@@ -19,7 +54,7 @@ class Customer(db.Model):
         self.pesel = pesel
         self.street = street
         self.appNo = appNo
-        print("Getting: " + str(self),flush=True)
+        logger.success("Getting: " + str(self))
 
     def __repr__(self):
         return f"Customer(ID: {self.id}, Name: {self.name}, City: {self.city}, Age: {self.age}, Pesel: {self.pesel}, Street: {self.street}, AppNo: {self.appNo})"
